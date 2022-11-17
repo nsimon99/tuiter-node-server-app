d7d752c0d0ec649b0f6c5346fb5031b15c66df52
nsimon99 <simon.n@husky.neu.edu>
2022-11-17 15:59:41 -0500

    hello

--- .bash_profile
+++ .bash_profile
@@ -1,1 +1,1 @@
-export DB_CONNECTION_STRING="mongodb+srv://nehasimon:<kramersweety1999>@cluster0.tbzhhbg.mongodb.net/?retryWrites=true&w=majority"
+export DB_CONNECTION_STRING=mongodb+srv://nehasimon:<kramer>@cluster0.ycn6rkn.mongodb.net/?retryWrites=true&w=majority
--- app.js
+++ app.js
@@ -21,11 +21,12 @@ app.listen(process.env.PORT || 4000);
 
 import mongoose from "mongoose";
 
-const uri = encodeURIComponent("mongodb+srv://nehasimon:<kramer>@cluster0.ycn6rkn.mongodb.net/?retryWrites=true&w=majority");
+//const uri = encodeURIComponent("mongodb+srv://nehasimon:<kramer>@cluster0.ycn6rkn.mongodb.net/?retryWrites=true&w=majority");
 
 
-const CONNECTION_STRING = uri
-    || 'mongodb://localhost:27017/tuiter'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter/';
 mongoose.connect(CONNECTION_STRING);
+
 
 
