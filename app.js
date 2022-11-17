8502636f83d2a4a2c68a39050b7ec8f4431c9d71
nsimon99 <simon.n@husky.neu.edu>
2022-11-17 17:44:30 -0500

    deleted bash file

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
 import express from 'express';
 import cors from 'cors';
 const app = express();
 app.use(cors());
-
-
-
 
 import HelloController
     from "./controllers/hello-controller.js";
@@ -18,23 +21,4 @@ TuitsController(app);
 HelloController(app);
 UserController(app);
 app.listen(process.env.PORT || 4000);
-/*
-import { createRequire } from 'module';
 
-const require = createRequire(import.meta.url);
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = encodeURIComponent("mongodb+srv://nehasimon:<kramer>@cluster0.ycn6rkn.mongodb.net/?retryWrites=true&w=majority");
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
-
-
-*/
-import mongoose from "mongoose";
-
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://localhost:27017/tuiter'
-mongoose.connect(CONNECTION_STRING);
